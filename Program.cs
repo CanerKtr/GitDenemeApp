8b473481220e4bcf62fdd2dc8fd1b4fd3557db81
Salim Caner Kitir <ckitir.dev@gmail.com>
2025-11-10 16:16:46 +0300

    Başkabirprojedenpullandpush

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello User");
+            Console.WriteLine("geriye doğru bir committen branch oluşturdum. Ve onu github'a pushlayacağım daha sonra ana projeyi açıp oradan pull etmeye çalışacağım");
 
         }
     }
