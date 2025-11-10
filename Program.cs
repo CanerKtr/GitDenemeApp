cae2e0cc89266c67e9ee7af9ce0e2ef94f38778c
Salim Caner Kitir <ckitir.dev@gmail.com>
2025-11-10 16:54:13 +0300

    AnaProjedenPushlayıncaPRistiyormudeneme

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello User");
             Console.WriteLine("geriye doğru bir committen branch oluşturdum. Ve onu github'a pushlayacağım daha sonra ana projeyi açıp oradan pull etmeye çalışacağım");
+            Console.WriteLine("yeni bir satır ekledim ana projeden bakalım pr isteyecek mi");
 
         }
     }
