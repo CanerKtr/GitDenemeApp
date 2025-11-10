bdfd6be18485b601bdc1c67eaf4f5d3e93d2e1d1
Salim Caner Kitir <ckitir.dev@gmail.com>
2025-11-10 17:11:45 +0300

    Branch2commitlendi

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@
             Console.WriteLine("Hello User");
             Console.WriteLine("geriye doğru bir committen branch oluşturdum. Ve onu github'a pushlayacağım daha sonra ana projeyi açıp oradan pull etmeye çalışacağım");
             Console.WriteLine("yeni bir satır ekledim ana projeden bakalım pr isteyecek mi");
+            Console.WriteLine("Branch2 yide oluşturdum bakalım nasıl olacak.");
+            Console.WriteLine("Branch2 nin branch1den farkı bu satır olacak.");
 
         }
     }
